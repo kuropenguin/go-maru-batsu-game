711326118ef9d2008fc1d2b3b3edad02573328c0
kuropenguin <kuro1994.t@gmail.com>
2023-02-14 20:20:15 +0900

    入力を受け付け

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
 
 var Board = [][]string{
-	[]string{"■", "■", "■"},
-	[]string{"■", "■", "■"},
-	[]string{"■", "■", "■"},
+	[]string{"", "1", "2", "3"},
+	[]string{"1", "■", "■", "■"},
+	[]string{"2", "■", "■", "■"},
+	[]string{"3", "■", "■", "■"},
 }
 
 func main() {
-	for _, row := range Board {
-		fmt.Println(row)
+	for {
+		for _, row := range Board {
+			fmt.Println(row)
+		}
+
+		fmt.Println("Please enter the row, col(1~3). For example: 1,3")
+		reader := bufio.NewReader(os.Stdin)
+		text, err := reader.ReadString('\n')
+		if err != nil && err != io.EOF {
+			fmt.Println(err)
+			fmt.Println("もう一度入力してください")
+			continue
+		}
+		fmt.Println(text)
 	}
+	fmt.Println("Game Done")
 }
