956d1d9e57e482badc2af8dc942c1e22929a43cf
kuropenguin <kuro1994.t@gmail.com>
2023-02-14 20:37:20 +0900

    1~3の数値のみ入力可能に

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
+	"strings"
 )
 
 var Board = [][]string{
@@ -22,13 +24,43 @@ func main() {
 
 		fmt.Println("Please enter the row, col(1~3). For example: 1,3")
 		reader := bufio.NewReader(os.Stdin)
-		text, err := reader.ReadString('\n')
+		input, err := reader.ReadString('\n')
 		if err != nil && err != io.EOF {
 			fmt.Println(err)
 			fmt.Println("もう一度入力してください")
 			continue
 		}
-		fmt.Println(text)
+
+		input = strings.Trim(input, "\n")
+		inputArr := strings.Split(input, ",")
+		if len(inputArr) > 2 {
+			fmt.Println("入力が多すぎます。もう一度入力してください")
+			continue
+		}
+		if len(inputArr) < 2 {
+			fmt.Println("入力が少なすぎます。もう一度入力してください")
+			continue
+		}
+		fmt.Println(inputArr)
+
+		row, err := strconv.Atoi(inputArr[0])
+		if err != nil {
+			fmt.Println("rowの入力で数字以外が使用されました。もう一度入力してください")
+			continue
+		}
+		if !(1 <= row && row <= 3) {
+			fmt.Println("rowの入力は1~3の間でのみ可能です。もう一度入力してください")
+			continue
+		}
+		col, err := strconv.Atoi(inputArr[1])
+		if err != nil {
+			fmt.Println("colの入力で数字以外が使用されました。もう一度入力してください")
+			continue
+		}
+		if !(1 <= col && col <= 3) {
+			fmt.Println("colの入力は1~3の間でのみ可能です。もう一度入力してください")
+			continue
+		}
 	}
 	fmt.Println("Game Done")
 }
